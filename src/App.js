da9f469482a2bbe326c9b0c5132c41b8f892f5ab
Muhammad Abdullah <101110601+mabdullahjs@users.noreply.github.com>
2023-08-25 13:34:52 +0500

    project initialization

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1 className="text-3xl text-center mt-2">
+        Weather App
+      </h1>
+      <div className='w-[50%] mx-auto mt-[3%]'>
+        <input className="focus:ring-2 focus:ring-blue-500 focus:outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md py-2 pl-10 ring-1 ring-slate-200 shadow-sm" type="text" aria-label="Filter projects" placeholder="City Name" />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
