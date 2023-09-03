2dc9842537173a2b50b99275d931c3079caebf56
usman <usman2037966@gmail.com>
2023-09-03 10:56:07 +0500

    problem solved

--- src/App.js
+++ src/App.js
@@ -6,10 +6,6 @@ const App = () => {
   return (
     <div >
       <Weather />
-      {/* <button
-        class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-        Button
-      </button> */}
     </div>
   )
 }
